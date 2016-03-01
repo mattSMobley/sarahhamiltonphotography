ba912d4a1ea2b00ad9b5a10dcdebc263c353abab
Matt Mobley <mattsmobley@gmail.com>
2016-03-01 13:31:06 -0700

    almost got the Angular version working, just need to fix the main page jquery gallery

--- js/routes.js
+++ js/routes.js
@@ -4,21 +4,6 @@ angular.module("app")
       .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainController as MC'
-      })
-      .when('/aroundtown',{
-        templateUrl: 'views/aroundtown.html',
-        controller: 'ATController as ATC'
-      })
-      .when('/sunsets', {
-        templateUrl: 'views/sunsets.html',
-        controller: 'SunsetsController as SC'
-      })
-      .when('/parks', {
-        templateUrl: 'views/parks.html',
-        controller: 'ParksController as PC'
-      })
-      .when('/trips', {
-        templateUrl: 'views/trips.html',
-        controller: 'TripsController as TC'
       });
+
 });
